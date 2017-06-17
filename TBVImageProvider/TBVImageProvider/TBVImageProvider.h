9627084681a2e93fe21b3be69d794badc3624e57
tripleCC <songruiwang@tripleCC.lan>
2017-06-17 15:19:56 +0800

    add public header file

---
+++ TBVImageProvider/TBVImageProvider/TBVImageProvider.h
@@ -0,0 +1,18 @@
+//
+//  TBVImageProvider.h
+//  TBVImageProvider
+//
+//  Created by tripleCC on 2017/6/17.
+//  Copyright © 2017年 tripleCC. All rights reserved.
+//
+
+#ifndef TBVImageProvider_h
+#define TBVImageProvider_h
+
+#import "TBVLocalImageProvider.h"
+#import "TBVWebImageProvider.h"
+#import "TBVAssetImageProvider.h"
+#import "TBVAssetsReformer.h"
+#import "NSError+TBVImageProvider.h"
+
+#endif /* TBVImageProvider_h */
